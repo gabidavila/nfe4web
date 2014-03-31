844d6c91ed43899ae495bd805a6c5e4ed7bdbb18
Gabriela D'Avila <gabidavila@gmail.com>
2014-03-31 14:51:32 -0300

    Versao inicial 1.0

---
+++ app/code/community/Mage/Adminhtml/Model/System/Config/Source/Diyoptions136409206524.php
@@ -0,0 +1,20 @@
+<?php
+class Mage_Adminhtml_Model_System_Config_Source_Diyoptions136409206524
+{
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+		
+            array('value' => 1, 'label'=>Mage::helper('adminhtml')->__('1')),
+            array('value' => 2, 'label'=>Mage::helper('adminhtml')->__('2')),
+            array('value' => 3, 'label'=>Mage::helper('adminhtml')->__('3')),
+        );
+    }
+
+}
